673e8eec23ef0977d671cea16d3ad0219ea0a908
ntesic <nikolatesic@gmail.com>
2019-01-29 22:16:55 +0100

    first commit

---
+++ src/Bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace aki2013\yiiboilerplate;
+
+use yii\base\Application;
+
+class Bootstrap implements BootstrapInterface
+{
+    
+}
---
+++ src/Module.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace aki2013\yiiboilerplate;
+
+use yii\base\Module as BaseModule;
+use Yii;
+
+
+class Module extends BaseModule
+{
+    /**
+     * Version of the module
+     */
+    const VERSION = '1.0.86';
+
+    /**
+     * Init process used to add the config of this package to the app
+     */
+    public function init()
+    {
+        parent::init();
+    }
+
+}
