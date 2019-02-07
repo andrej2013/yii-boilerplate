8d4b0f1aa485a4079440de06fd56e2285588a508
ntesic <nikolatesic@gmail.com>
2019-02-07 23:27:19 +0100

    grid config rbac

---
+++ src/migrations/m190207_214706_grid_config_auth_rights.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Schema;
+use andrej2013\yiiboilerplate\Migration;
+
+class m190207_214706_grid_config_auth_rights extends Migration
+{
+    public function up()
+    {
+        $auth = $this->getAuth();
+        $authenticated = $auth->createRole('Authenticated');
+        $auth->add($authenticated);
+        $permission = $auth->createPermission('backend_grid-config');
+        $auth->add($permission);
+        $auth->addChild($authenticated, $permission);
+    }
+
+    public function down()
+    {
+        echo "m190207_214706_grid_config_auth_rights cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
