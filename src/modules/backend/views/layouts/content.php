ee61d3b04a9ae167ac2ab82800049ba14693d7df
ntesic <nikolatesic@gmail.com>
2019-02-17 15:25:37 +0100

    Footer

--- src/modules/backend/views/layouts/content.php
+++ src/modules/backend/views/layouts/content.php
@@ -45,7 +45,7 @@ use dmstr\widgets\Alert;
     <div class="pull-right hidden-xs">
         <b>Version</b> 2.0
     </div>
-    <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2018-<?php echo date("Y", time()) ?> <a href="<?php echo Yii::$app->homeUrl; ?>"><?php echo getenv('APP_TITLE'); ?></a>.</strong> All rights
     reserved.
 </footer>
 
